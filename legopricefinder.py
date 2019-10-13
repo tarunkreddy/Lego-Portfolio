1f7b91da175a3c2263f7112cafe724be6691d8ed
Teddy <tarunkreddy@gmail.com>
2019-10-12 22:15:35 -0700

    functions to add new sets and check set price

--- legopricefinder.py
+++ legopricefinder.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from collections import namedtuple
+import sqlite3
+import sys
+
+
+Lego_Set = namedtuple('Lego_Set', ['times_sold', 'total_qty', 'min_price', 'avg_price', 'qty_avg_price', 'max_price'])
 
 #creates the Lego_Set named tuple from row data
-def createTuples(table):
+def getSetData(table):
 	rows = table.find_all('td')
 
 	clean_rows = []
@@ -18,29 +23,81 @@ def createTuples(table):
 		float(clean_rows[11][4:])
 	)
 
+def addSet():
+	lego_id = int(input("Enter Product ID: "))
+	purchase_price = float(input("Enter Purchase Price: "))
+	actual_selling_price = float(input("Enter sale price, 0 if not sold: "))
+	shipping_cost = float(input("Enter shipping cost: "))
+	estimated_selling_price = checkPrice(lego_id)
+	conn = sqlite3.connect('lms.db')
+	c = conn.cursor()
+	sql_insert_set = """ INSERT INTO collection VALUES (?,?,?,?,?,?); """
+	c.execute(sql_insert_set, (lego_id, 
+								'placeholder name',
+							 	purchase_price,
+							 	estimated_selling_price,
+							 	actual_selling_price,
+							 	shipping_cost)
+			)
+	conn.commit()
+	conn.close()
+	return
+
+def checkPrice(lego_id=-1):
+	if (lego_id == -1):
+		lego_id = int(input("Enter Product ID: "))
+	url = 'http://www.bricklink.com/v2/catalog/catalogitem.page?S='+str(lego_id)+'#T=P'
+	browser = webdriver.Chrome()
+	browser.get(url)
+	source = browser.page_source
+
+	soup = BeautifulSoup(source, 'lxml')
+
+	table = soup.find('table', class_='pcipgSummaryTable')
+
+	browser.quit()
+
+	return getSetData(table).avg_price
+
+
+def main():
+	num_args = len(sys.argv) - 1
+	args = sys.argv[1:]
+	if (num_args == 0):
+		print 'No arguments given'
+		return
+	elif (num_args == 1):
+
+		if (args[0] == 'add'):
+			addSet()
+		elif (args[0] == 'check'):
+			print checkPrice()
+		else:
+			return
+	else:
+		return
+
+if __name__ == '__main__':
+	main()
+
+# conn = sqlite3.connect('lms.db')
+
+# Lego_Set = namedtuple('Lego_Set', ['times_sold', 'total_qty', 'min_price', 'avg_price', 'qty_avg_price', 'max_price'])
+
+# lego_id = str(input("Enter Product ID: "))
+# url = 'http://www.bricklink.com/v2/catalog/catalogitem.page?S='+lego_id+'#T=P'
+# browser = webdriver.Chrome()
+# browser.get(url)
+# source = browser.page_source
+
+# soup = BeautifulSoup(source, 'lxml')
+
+# table = soup.find('table', class_='pcipgSummaryTable')
 
 
 
-Lego_Set = namedtuple('Lego_Set', ['times_sold', 'total_qty', 'min_price', 'avg_price', 'qty_avg_price', 'max_price'])
-
-lego_id = str(input("Enter Product ID: "))
-url = 'http://www.bricklink.com/v2/catalog/catalogitem.page?S='+lego_id+'#T=P'
-browser = webdriver.Chrome()
-browser.get(url)
-source = browser.page_source
-
-# f = open("results_"+lego_id+".txt", "w")
-
-# f.write(source)
-# f.close()
-
-soup = BeautifulSoup(source, 'lxml')
-
-table = soup.find('table', class_='pcipgSummaryTable')
-
-
-print createTuples(table)
-browser.quit()
+# browser.quit()
+# conn.close()
 
 
 
--- setup_db.py
+++ setup_db.py
@@ -10,7 +10,8 @@ sql_create_collection_table = """ CREATE TABLE IF NOT EXISTS collection (
                                         name text,
                                         purchase_price real,
                                         estimated_selling_price real,
-                                        actual_selling_price real
+                                        actual_selling_price real,
+                                        shipping_cost real
 
 
                                     ); """
