54bce43ff9e8ac786646a6ce311d714bc16d2544
Teddy <tarunkreddy@gmail.com>
2019-10-12 21:46:47 -0700

    fixed typo

--- setup_db.py
+++ setup_db.py
@@ -10,7 +10,7 @@ sql_create_collection_table = """ CREATE TABLE IF NOT EXISTS collection (
                                         name text,
                                         purchase_price real,
                                         estimated_selling_price real,
-                                        actual_selling_price real,
+                                        actual_selling_price real
 
 
                                     ); """
