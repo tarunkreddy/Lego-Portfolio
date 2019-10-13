2da28fba510743ac1a67e8004ae4bf02a8ed6bc2
Teddy <tarunkreddy@gmail.com>
2019-10-13 13:02:41 -0700

    changed lego_id to charfield for comcon sets

--- lcm/collection/models.py
+++ lcm/collection/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class LegoSet(models.Model):
-	lego_id = models.IntegerField(default=0, primary_key=True)
+	lego_id = models.CharField(default='0', max_length=20, primary_key=True)
 	set_name = models.CharField(default='placeholder_name', max_length=100)
 	estimated_selling_price = models.DecimalField(max_digits=6, decimal_places=2)
 	price_last_updated = models.DateField(auto_now=True)
@@ -13,7 +13,7 @@ class LegoSet(models.Model):
 	
 
 	def __str__(self):
-		return str(self.lego_id) + '-' + self.set_name
+		return self.lego_id + '-' + self.set_name
 
 class CollectionItem(models.Model):
 	owner = models.CharField(max_length=20, primary_key=True)
@@ -23,5 +23,5 @@ class CollectionItem(models.Model):
 	shipping_cost = models.DecimalField(max_digits=5, decimal_places=2)
 
 	def __str__(self):
-		return self.owner + "'s " + str(self.lego_id) + ': Paid ' + str(self.purchase_price+self.shipping_cost) + ', Sold for ' + str(self.actual_selling_price) 
+		return self.owner + "'s " + self.lego_id + ': Paid ' + str(self.purchase_price+self.shipping_cost) + ', Sold for ' + str(self.actual_selling_price) 
 
